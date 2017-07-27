2924deed117d23596ddfce47a30a64b26bbbbcc4
Thomas Weißschuh <thomas.weissschuh@de.amadeus.com>
2017-07-27 12:44:23 +0200

    remove unused import

--- src/main/java/org/sonar/plugins/coverageevolution/CoveragePlugin.java
+++ src/main/java/org/sonar/plugins/coverageevolution/CoveragePlugin.java
@@ -3,7 +3,6 @@ package org.sonar.plugins.coverageevolution;
 import java.util.Arrays;
 import java.util.List;
 import org.sonar.api.SonarPlugin;
-import org.sonar.plugins.coverageevolution.client.DefaultSonarClient;
 import org.sonar.plugins.coverageevolution.client.SonarClient;
 
 public class CoveragePlugin extends SonarPlugin {
